762e3a311dd85da6b1b45d866534fea49b1c7543
hugcis <hmj.cisneros@gmail.com>
2022-05-19 08:35:48 +0000

    Fix javascript file location

--- static/map/map.js
+++ static/map/map.js
@@ -1,5 +1,5 @@
 var getUrl = window.location;
-var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+var baseUrl = getUrl.protocol + "//" + getUrl.host;
 var map = L.map('map').setView([39.74739, -105], 13);
 
 var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
