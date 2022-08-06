7bae32b54291bd294743cf065bdcaab10e2b43fb
hugcis <hmj.cisneros@gmail.com>
2022-08-06 21:06:53 +0200

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./static/**/*.{html,js}", "./templates/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
